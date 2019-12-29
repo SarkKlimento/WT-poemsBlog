dc822c1aebfa2ec8b8cd4c349b15244d048dd2e6
SarkKlimento <47054290+SarkKlimento@users.noreply.github.com>
2019-12-29 18:01:06 +0300

    Initial commit

---
+++ js/articles.js
@@ -0,0 +1,47 @@
+function load() {
+    const data = [
+        {
+            index: 'someid',
+            header: 'Some header',
+            body: 'Some body',
+            comments: '77',
+            postDate: '07.07.19'
+        },
+        {
+            index: 'someid',
+            header: 'Some header',
+            body: 'Some body',
+            comments: '77',
+            postDate: '07.07.19'
+        },
+        {
+            index: 'someid',
+            header: 'Some header',
+            body: 'Some body',
+            comments: '77',
+            postDate: '07.07.19'
+        },
+        {
+            index: 'someid',
+            header: 'Some header',
+            body: 'Some body',
+            comments: '77',
+            postDate: '07.07.19'
+        }
+    ];
+
+    data.forEach(post => createPost(post));
+}
+
+function createPost(postContent) {
+    let postElement = '<div class="col-12 col-md-9 row post"><p class="post-header">|1|</p><p class="post-body">|2|</p><div class="col-12 row no-margin"><div class="col"><p>|3|</p></div><div class="col"><p class="post-footer">|4|</p></div></div></div>\n';
+
+    $("#posts").append($(
+        postElement.replace("|1|", postContent.header)
+            .replace("|2|", postContent.body)
+            .replace("|3|", 'Комментариев: ' + postContent.comments)
+            .replace("|4|", postContent.postDate)
+    ));
+}
+
+load();
